c0e3d912b2c4a394d6b2c7fff3b93e03b840922a
Rafael Melo <rafaelnunesmelo@gmail.com>
2024-07-09 10:28:53 -0300

    Criando webservice

--- TempoBom/TempoBom/Controllers/AddWeatherCityViewController.swift
+++ TempoBom/TempoBom/Controllers/AddWeatherCityViewController.swift
@@ -2,10 +2,20 @@ import UIKit
 
 class AddWeatherCityViewController: UIViewController {
     
-    @IBOutlet weak var cityTextLabel: UILabel!
+    @IBOutlet weak var cityTextLabel: UITextField!
     
     @IBAction func saveCityButtonPressed() {
-        
+        if let cityName = cityTextLabel.text,
+           let weatherURL = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&units=metric&appid=23d4c5080cb5ea567d3ace39f4538feb"){
+            
+            let weatherResource = Resource<Any>(url: weatherURL) { data in
+                return data
+            }
+            
+            Webservice().load(resource: weatherResource) { result in
+                
+            }
+        }
     }
     
     @IBAction func cancelButtonPressed() {
---
+++ TempoBom/TempoBom/Services/WebService.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+struct Resource<T> {
+    let url: URL
+    let parse: (Data) -> T?
+}
+
+final class Webservice {
+    func load<T>(resource: Resource<T>, completion: @escaping (T?) -> ()) {
+        URLSession.shared.dataTask(with: resource.url) { data, response, error in
+            print(data?.prettyPrintedJSON ?? "")
+            if let data = data {
+                DispatchQueue.main.async {
+                    completion(resource.parse(data))
+                }
+            } else {
+                completion(nil)
+            }
+            
+        }.resume()
+    }
+}
+
+extension Data {
+    var prettyPrintedJSON: NSString? {
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+              let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
+
+        return prettyPrintedString
+    }
+}
